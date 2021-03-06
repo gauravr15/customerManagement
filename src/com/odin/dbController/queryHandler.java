c2d69290f3fecf003ba78e532764fb5c9bf32903
gaurav bhasker <gauravbhasker8@gmail.com>
2021-03-06 22:23:18 +0530

    heesabKhaata6thFeb

--- src/com/odin/dbController/queryHandler.java
+++ src/com/odin/dbController/queryHandler.java
@@ -1,18 +1,24 @@
 package com.odin.dbController;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
 
 
-public class queryHandler {
+public class queryHandler extends HttpServlet{
 	
 	Logger LOG = Logger.getLogger(queryHandler.class.getClass());
-	
+	HttpServletResponse res = null;
 	public boolean loginHandler(String user, String pass) {
 		LOG.debug("Inside login handler.");
 		boolean userAuth = false;
@@ -21,8 +27,8 @@ public class queryHandler {
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
-			stmt = conn.createStatement();
 			String query = "SELECT * FROM USER_AUTH WHERE USER = '"+user+"' AND PASS = '"+pass+"';";
+			stmt = conn.createStatement();
 			LOG.debug("query to fire is "+query);
 			rs = stmt.executeQuery(query);
 			int count =0;
@@ -55,5 +61,105 @@ public class queryHandler {
 			}
 		}
 		return userAuth;
+	}
+	
+	public void billingHandler(String mob,String bill) {
+		dbSetup dbObj = new dbSetup();
+		Connection conn = dbObj.dbInit();
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			String query = "SELECT * FROM CUSTOMER_INFO WHERE mobile_no = '"+mob+"';";
+			stmt = conn.createStatement();
+			LOG.info("query to fire is "+query);
+			rs = stmt.executeQuery(query);
+			if(rs.next()) {
+				String total_point = rs.getString("TOTAL_POINTS");
+				LOG.info("Total points for user :"+mob+" is :"+total_point);
+				int calc_point = Integer.parseInt(bill)/10;
+				total_point = Integer.toString(Integer.parseInt(total_point)+calc_point);
+				LOG.info("Points added for this transaction is : "+calc_point);
+				LOG.info("Total points for user : "+mob+" is : "+total_point);
+				query = "UPDATE CUSTOMER_INFO SET TOTAL_POINTS = '"+total_point+"' WHERE mobile_no = '"+mob+"';";
+				LOG.info("Query to fire : "+query);
+				if(stmt.executeUpdate(query)!=0) {
+					LOG.info("Points updated successfully");
+				}
+				else {
+					LOG.error("Failed to update points");
+				}
+			}
+			else {
+				LOG.info("customer doesn't exist");
+			}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			LOG.error("Unable to create statement.");
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				LOG.debug("Releasing db connection");
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				LOG.error("Failed to release connection.");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void billingHandler(String mob,String bill, String point) {
+		dbSetup dbObj = new dbSetup();
+		Connection conn = dbObj.dbInit();
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			String query = "SELECT * FROM CUSTOMER_INFO WHERE mobile_no = '"+mob+"';";
+			stmt = conn.createStatement();
+			LOG.info("query to fire is "+query);
+			rs = stmt.executeQuery(query);
+			if(rs.next()) {
+				int total_point = Integer.parseInt(rs.getString("TOTAL_POINTS"));
+				LOG.info("Total points for user :"+mob+" is :"+total_point);
+				if(total_point < Integer.parseInt(point)) {
+					LOG.debug("Insufficient points");
+					billingHandler(mob,bill);
+				}
+				else {
+					int calc_point = total_point - Integer.parseInt(point);
+					query = "UPDATE CUSTOMER_INFO SET TOTAL_POINTS = '"+calc_point+"' WHERE mobile_no = '"+mob+"';";
+					if(stmt.executeUpdate(query)!=0){
+						LOG.info("Points deducted successfully");
+					}
+					else {
+						LOG.info("Points deduction failed");
+					}
+				}
+			}
+			else {
+				LOG.error("user doesn't exists");
+			}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			LOG.error("Unable to create statement.");
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				LOG.debug("Releasing db connection");
+				rs.close();
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				LOG.error("Failed to release connection.");
+				e.printStackTrace();
+			}
+		}
 	}
 }
---
+++ src/com/odin/servlet/filter/homeFilter.java
@@ -0,0 +1,69 @@
+package com.odin.servlet.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+
+
+/**
+ * Servlet Filter implementation class homeFilter
+ */
+@WebFilter({"/home.html","/billing.html"})
+
+public class homeFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+	
+	Logger LOG = Logger.getLogger(homeFilter.class.getClass());
+	
+    public homeFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		LOG.debug(session.getAttribute("user"));
+		if(session.getAttribute("user")!=null) {
+			LOG.debug("user logged in");
+			chain.doFilter(request, response);
+		}
+		else {
+			HttpServletResponse res = (HttpServletResponse)response;
+			res.sendRedirect("http://localhost:8080/Subscription/login.html");
+			LOG.error("user needs to log in first");
+		}
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
